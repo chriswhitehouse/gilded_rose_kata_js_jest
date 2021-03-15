746b6f4ceca727d5714510c8637d616a708ea254
Chris Whitehouse <chris_whitehouse@hotmail.com>
2021-03-15 18:06:04 +0000

    implment full feature test

---
+++ test/feature.test.js
@@ -0,0 +1,84 @@
+const {Shop, Item} = require("../src/gilded_rose");
+
+const items = [
+      new Item('apple', 1, 3)
+    ];
+
+const shop = new Shop(items)
+
+describe("So that I can keep track of my stock", function() {
+  it("I want to maint an inventory of items", function() {
+    expect(items[0].name).toBe('apple');
+  });
+});
+
+describe("So that I know when I need to sell an item by", function() {
+  it("I want each item to have a 'SellIn' property", function() {
+    expect(items[0].sellIn).toBe(1);
+  });
+});
+
+describe("So that I know the value of an item when I sell it", function() {
+  it("I want each item to have a 'Quality' property", function() {
+    expect(items[0].quality).toBe(3);
+  });
+});
+
+describe("So that I can maintain a current view of quality and sellIn", function() {
+  it("I want to be able to update quality of every item in the inventory at the end of each day", function() {
+    shop.updateQuality()
+    expect(shop.items[0].sellIn).toBe(0);
+    expect(shop.items[0].quality).toBe(2);
+  });
+});
+
+describe("Manages the quality of an inventory over time", function() {
+  const inventoryItems = [
+        new Item('apple', 1, 3),
+        new Item('banana', 2, 4),
+        new Item('Aged Brie', 1, 5),
+        new Item('Sulfuras, Hand of Ragnaros', 1, 80),
+        new Item('Backstage passes to a TAFKAL80ETC concert', 11, 5)
+      ];
+
+  const inventory = new Shop(inventoryItems)
+
+  it("Normal items quality decrease by 1 before sellIn date + 'Aged Brie' actually increases in Quality the older it gets", function() {
+    inventory.updateQuality()
+    expect(inventory.items[0].quality).toBe(2)
+    expect(inventory.items[1].quality).toBe(3)
+    expect(inventory.items[2].quality).toBe(6)
+    expect(inventory.items[3].quality).toBe(80)
+    expect(inventory.items[4].quality).toBe(6)
+  })
+
+  it("Once the sell by date has passed, Quality degrades twice as fast + BACKSTAGE PASS Quality increases by 2 with less than 10 sellIn days", function() {
+    inventory.updateQuality()
+    expect(inventory.items[0].quality).toBe(0)
+    expect(inventory.items[4].quality).toBe(8)
+  })
+
+  it("BACKSTAGE PASS Quality increases by 3 with less than 5 sellIn days + The Quality of an item is never negative", function() {
+    for (let i = 0; i < 5; i++) {
+      inventory.updateQuality()
+    }
+    expect(inventory.items[0].quality).toBe(0)
+    expect(inventory.items[4].quality).toBe(19)
+  })
+
+  it("BACKSTAGE PASS Quality drops to 0 after the concert", function() {
+    for (let i = 0; i < 5; i++) {
+      inventory.updateQuality()
+    }
+    expect(inventory.items[4].quality).toBe(0)
+  })
+
+  it("The Quality of an item is never more than 50", function() {
+    for (let i = 0; i < 12; i++) {
+      inventory.updateQuality()
+    }
+    expect(inventory.items[2].quality).not.toBeGreaterThan(50)
+  })
+
+
+})
