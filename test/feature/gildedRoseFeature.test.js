312f35262d3b959381e221dbe915251d7fc4320d
Chris Whitehouse <chris_whitehouse@hotmail.com>
2021-03-15 20:12:38 +0000

    rename variables to gildedRose in Feature test

--- test/feature/gildedRoseFeature.test.js
+++ test/feature/gildedRoseFeature.test.js
@@ -8,7 +8,7 @@ const items = [
 const shop = new Shop(items)
 
 describe("So that I can keep track of my stock", function() {
-  it("I want to maint an inventory of items", function() {
+  it("I want to maint an gildedRose of items", function() {
     expect(items[0].name).toBe('apple');
   });
 });
@@ -33,8 +33,8 @@ describe("So that I can maintain a current view of quality and sellIn", function
   });
 });
 
-describe("Manages the quality of an inventory over time", function() {
-  const inventoryItems = [
+describe("Manages the quality of an gildedRose over time", function() {
+  const gildedRoseItems = [
         new Item('apple', 1, 3),
         new Item('banana', 2, 4),
         new Item('Aged Brie', 1, 5),
@@ -42,43 +42,43 @@ describe("Manages the quality of an inventory over time", function() {
         new Item('Backstage passes to a TAFKAL80ETC concert', 11, 5)
       ];
 
-  const inventory = new Shop(inventoryItems)
+  const gildedRose = new Shop(gildedRoseItems)
 
   it("Normal items quality decrease by 1 before sellIn date + 'Aged Brie' actually increases in Quality the older it gets", function() {
-    inventory.updateQuality()
-    expect(inventory.items[0].quality).toBe(2)
-    expect(inventory.items[1].quality).toBe(3)
-    expect(inventory.items[2].quality).toBe(6)
-    expect(inventory.items[3].quality).toBe(80)
-    expect(inventory.items[4].quality).toBe(6)
+    gildedRose.updateQuality()
+    expect(gildedRose.items[0].quality).toBe(2)
+    expect(gildedRose.items[1].quality).toBe(3)
+    expect(gildedRose.items[2].quality).toBe(6)
+    expect(gildedRose.items[3].quality).toBe(80)
+    expect(gildedRose.items[4].quality).toBe(6)
   })
 
   it("Once the sell by date has passed, Quality degrades twice as fast + BACKSTAGE PASS Quality increases by 2 with less than 10 sellIn days", function() {
-    inventory.updateQuality()
-    expect(inventory.items[0].quality).toBe(0)
-    expect(inventory.items[4].quality).toBe(8)
+    gildedRose.updateQuality()
+    expect(gildedRose.items[0].quality).toBe(0)
+    expect(gildedRose.items[4].quality).toBe(8)
   })
 
   it("BACKSTAGE PASS Quality increases by 3 with less than 5 sellIn days + The Quality of an item is never negative", function() {
     for (let i = 0; i < 5; i++) {
-      inventory.updateQuality()
+      gildedRose.updateQuality()
     }
-    expect(inventory.items[0].quality).toBe(0)
-    expect(inventory.items[4].quality).toBe(19)
+    expect(gildedRose.items[0].quality).toBe(0)
+    expect(gildedRose.items[4].quality).toBe(19)
   })
 
   it("BACKSTAGE PASS Quality drops to 0 after the concert", function() {
     for (let i = 0; i < 5; i++) {
-      inventory.updateQuality()
+      gildedRose.updateQuality()
     }
-    expect(inventory.items[4].quality).toBe(0)
+    expect(gildedRose.items[4].quality).toBe(0)
   })
 
   it("The Quality of an item is never more than 50", function() {
     for (let i = 0; i < 12; i++) {
-      inventory.updateQuality()
+      gildedRose.updateQuality()
     }
-    expect(inventory.items[2].quality).not.toBeGreaterThan(50)
+    expect(gildedRose.items[2].quality).not.toBeGreaterThan(50)
   })
 
 
