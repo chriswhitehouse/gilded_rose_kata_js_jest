8f92cf8deddda98bbcecd388e1d5dd01c1668055
Chris Whitehouse <chris_whitehouse@hotmail.com>
2021-03-15 20:25:39 +0000

    refactor the logic into the item class

--- test/unit/shop.test.js
+++ test/unit/shop.test.js
@@ -1,22 +1,23 @@
 const { Shop } = require('../../src/shop.js')
+const { Item } = require('../../src/item.js')
 
-const shop = new Shop([{name: 'normalItem', sellIn: 1, quality: 2}])
 
 describe("Shop", function () {
   it("should record items", function() {
+    const shop = new Shop([new Item('normalItem', 1, 2)])
     expect(shop.items[0].name).toBe('normalItem')
   })
 
   describe("#updateQuality", function() {
     it("should decrease quality of normal items by 1, before SellIn", function() {
-      const shop = new Shop([{name: 'normalItem', sellIn: 1, quality: 2}])
+      const shop = new Shop([new Item('normalItem', 1, 2)])
       shop.updateQuality()
       expect(shop.items[0].sellIn).toBe(0)
       expect(shop.items[0].quality).toBe(1)
     })
 
     it("should maintain quality of normal items at 0, after SellIn", function() {
-      const shop = new Shop([{name: 'normalItem', sellIn: 1, quality: 2}])
+      const shop = new Shop([new Item('normalItem', 1, 2)])
       shop.updateQuality()
       shop.updateQuality()
       shop.updateQuality()
@@ -25,13 +26,13 @@ describe("Shop", function () {
     })
 
     it("once sellIn date passes, quality of normal Items should degrade twice as fast", function() {
-      const shop = new Shop([{name: 'normalItem', sellIn: 0, quality: 5}])
+      const shop = new Shop([new Item('normalItem', 0, 5)])
       shop.updateQuality()
       expect(shop.items[0].quality).toBe(3)
     })
 
     it("quality should never go negative", function() {
-      const shop = new Shop([{name: 'normalItem', sellIn: 1, quality: 2}])
+      const shop = new Shop([new Item('normalItem', 1, 2)])
       for (let i = 0; i < 3; i++) {
         shop.updateQuality()
       }
@@ -39,7 +40,7 @@ describe("Shop", function () {
     })
 
     it("quality should never go above 50", function() {
-      const shop = new Shop([{name: 'normalItem', sellIn: 1, quality: 2}])
+      const shop = new Shop([new Item('normalItem', 1, 2)])
       for (let i = 0; i < 50; i++) {
         shop.updateQuality()
       }
@@ -47,7 +48,7 @@ describe("Shop", function () {
     })
 
     it("'Aged Brie' actually increases in Quality the older it gets", function() {
-      const shop = new Shop([{name: 'Aged Brie', sellIn: 1, quality: 5}])
+      const shop = new Shop([new Item('Aged Brie', 1, 5)])
       shop.updateQuality()
       expect(shop.items[0].quality).toBe(6)
       shop.updateQuality()
@@ -55,32 +56,32 @@ describe("Shop", function () {
     })
 
     it("'Sulfuras', being a legendary item, never has to be sold or decreases in Quality", function() {
-      const shop = new Shop([{name: 'Sulfuras, Hand of Ragnaros', sellIn: 0, quality: 80}])
+      const shop = new Shop([new Item('Sulfuras, Hand of Ragnaros', 0, 80)])
       shop.updateQuality()
       expect(shop.items[0].quality).toBe(80)
     })
 
     describe("'Backstage passes', like aged brie, increases in Quality as its SellIn value approaches", function() {
       it("Quality increases by 1 when there are more than 10 days", function() {
-        const shop = new Shop([{name: 'Backstage passes to a TAFKAL80ETC concert', sellIn: 11, quality: 5}])
+        const shop = new Shop([new Item('Backstage passes to a TAFKAL80ETC concert', 11, 5)])
         shop.updateQuality()
         expect(shop.items[0].quality).toBe(6)
       })
 
       it("Quality increases by 2 when there are 10 days or less", function() {
-        const shop = new Shop([{name: 'Backstage passes to a TAFKAL80ETC concert', sellIn: 8, quality: 5}])
+        const shop = new Shop([new Item('Backstage passes to a TAFKAL80ETC concert', 8, 5)])
         shop.updateQuality()
         expect(shop.items[0].quality).toBe(7)
       })
 
       it("Quality increases by 3 when there are 5 days or less", function() {
-        const shop = new Shop([{name: 'Backstage passes to a TAFKAL80ETC concert', sellIn: 4, quality: 5}])
+        const shop = new Shop([new Item('Backstage passes to a TAFKAL80ETC concert', 4, 5)])
         shop.updateQuality()
         expect(shop.items[0].quality).toBe(8)
       })
 
       it("Quality drops to 0 after the concert", function() {
-        const shop = new Shop([{name: 'Backstage passes to a TAFKAL80ETC concert', sellIn: 0, quality: 5}])
+        const shop = new Shop([new Item('Backstage passes to a TAFKAL80ETC concert', 0, 5)])
         shop.updateQuality()
         expect(shop.items[0].quality).toBe(0)
       })
