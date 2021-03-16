65345b7e7478f76a4e3f2393e772644e58fd5130
Chris Whitehouse <chris_whitehouse@hotmail.com>
2021-03-16 12:23:03 +0000

    isolate shop unit test from item class

--- test/unit/shop.test.js
+++ test/unit/shop.test.js
@@ -1,21 +1,17 @@
 const { Shop } = require('../../src/shop.js')
-const { Item } = require('../../src/item.js')
-
 
 describe("Shop", function () {
   it("should record items", function() {
-    const shop = new Shop([new Item('normalItem', 1, 2)])
+    const shop = new Shop([{name: 'normalItem', sellIn: 1, quality: 2}])
     expect(shop.items[0].name).toBe('normalItem')
   })
 
   describe("#updateQuality", function() {
     it("should update the qiality of all items", function() {
-      const shop = new Shop([new Item('normalItem', 1, 2), new Item('normalItemToo', 2, 5)])
+      const mockUpdateQuality = jest.fn()
+      const shop = new Shop([{name: 'mockItem', updateQuality: mockUpdateQuality}, {name: 'mockItemToo', updateQuality: mockUpdateQuality}])
       shop.updateQuality()
-      expect(shop.items[0].sellIn).toBe(0)
-      expect(shop.items[0].quality).toBe(1)
-      expect(shop.items[1].sellIn).toBe(1)
-      expect(shop.items[1].quality).toBe(4)
+      expect(mockUpdateQuality.mock.calls.length).toBe(2)
     })
   })
 })
