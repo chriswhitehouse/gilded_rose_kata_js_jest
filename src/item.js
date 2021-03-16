01890ec6b328e64a3f808aa208dbc49db1780dda
Chris Whitehouse <chris_whitehouse@hotmail.com>
2021-03-16 13:43:05 +0000

    isolate logic for each item name

--- src/item.js
+++ src/item.js
@@ -6,46 +6,70 @@ class Item {
   }
 
   updateQuality() {
-    if (this.name != 'Aged Brie' && this.name != 'Backstage passes to a TAFKAL80ETC concert' && this.name != 'Sulfuras, Hand of Ragnaros') {
-      if (this.quality > 0) {
-        this.quality = this.quality - 1;
-      }
-    } else {
+    switch (this.name) {
+      case 'Aged Brie':
+        this.updateAgedBrie();
+        break;
+      case 'Sulfuras, Hand of Ragnaros':
+        this.updateSulfuras();
+        break;
+      case 'Backstage passes to a TAFKAL80ETC concert':
+        this.updateBackstagePass();
+        break;
+      default:
+        this.updateOther();
+    }
+  }
+
+  updateAgedBrie() {
+    if (this.quality < 50) {
+      this.quality = this.quality + 1;
+    }
+
+    this.sellIn = this.sellIn - 1;
+
+    if (this.sellIn < 0) {
       if (this.quality < 50) {
         this.quality = this.quality + 1;
-        if (this.name == 'Backstage passes to a TAFKAL80ETC concert') {
-          if (this.sellIn < 11) {
-            this.quality = this.quality + 1;
-          }
-          if (this.sellIn < 6) {
-            this.quality = this.quality + 1;
-          }
-        }
       }
     }
-    if (this.name != 'Sulfuras, Hand of Ragnaros') {
-      this.sellIn = this.sellIn - 1;
-    }
-    if (this.sellIn < 0) {
-      if (this.name != 'Aged Brie') {
-        if (this.name != 'Backstage passes to a TAFKAL80ETC concert') {
-          if (this.quality > 0) {
-            if (this.name != 'Sulfuras, Hand of Ragnaros') {
-              this.quality = this.quality - 1;
-            }
-          }
-        } else {
-          this.quality = this.quality - this.quality;
-        }
-      } else {
-        if (this.quality < 50) {
-          this.quality = this.quality + 1;
-        }
-      }
-    }
+  }
+
+  updateSulfuras() {
 
   }
 
+  updateBackstagePass() {
+    if (this.quality < 50) {
+      this.quality = this.quality + 1;
+    }
+    if (this.sellIn < 11) {
+      this.quality = this.quality + 1;
+    }
+    if (this.sellIn < 6) {
+      this.quality = this.quality + 1;
+    }
+
+    this.sellIn = this.sellIn - 1;
+
+    if (this.sellIn < 0) {
+      this.quality = this.quality - this.quality;
+    }
+  }
+
+  updateOther() {
+    if (this.quality > 0) {
+      this.quality = this.quality - 1;
+    }
+
+    this.sellIn = this.sellIn - 1;
+
+    if (this.sellIn < 0) {
+      if (this.quality > 0) {
+          this.quality = this.quality - 1;
+      }
+    }
+  }
 }
 
 module.exports = {
